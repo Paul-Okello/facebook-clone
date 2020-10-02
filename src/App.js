a0727981cd411373cc2e0120226f67fb0a84c299
Paul Okello <paulokello273@gmail.com>
2020-10-02 10:15:16 +0300

    header component outline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Facebook clone</h1>
-      {/* <Header /> */}
+      <Header />
       {/* app body */}
       {/* <Sidebar /> */}
       {/* <Feed />  */}
