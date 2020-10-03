3a267cc37f081fdd0d559f144fa4f34705c8265b
Paul Okello <paulokello273@gmail.com>
2020-10-03 14:27:39 +0300

    story styling

---
+++ src/StoryReal.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Story from "./Story";
+import "./StoryReal.css";
+
+function StoryReal() {
+  return (
+    <div className="storyReal">
+      <Story
+        image="https://64.media.tumblr.com/0f19ab1c04e2ffe70859eee327100f2c/tumblr_ojvzfrEBpf1r80ikgo1_500.jpg"
+        profileSrc="https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"
+        title="Paul Okello"
+      />
+      <Story
+        image="https://64.media.tumblr.com/0f19ab1c04e2ffe70859eee327100f2c/tumblr_ojvzfrEBpf1r80ikgo1_500.jpg"
+        profileSrc="https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"
+        title="Paul Okello"
+      />
+      <Story
+        image="https://64.media.tumblr.com/0f19ab1c04e2ffe70859eee327100f2c/tumblr_ojvzfrEBpf1r80ikgo1_500.jpg"
+        profileSrc="https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"
+        title="Paul Okello"
+      />
+      <Story
+        image="https://64.media.tumblr.com/0f19ab1c04e2ffe70859eee327100f2c/tumblr_ojvzfrEBpf1r80ikgo1_500.jpg"
+        profileSrc="https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"
+        title="Paul Okello"
+      />
+      <Story
+        image="https://64.media.tumblr.com/0f19ab1c04e2ffe70859eee327100f2c/tumblr_ojvzfrEBpf1r80ikgo1_500.jpg"
+        profileSrc="https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"
+        title="Paul Okello"
+      />
+    </div>
+  );
+}
+
+export default StoryReal;
