d7fa6c1436db492b5ced01bb5e559b539c5b4ba3
Paul Okello <paulokello273@gmail.com>
2020-10-03 11:33:59 +0300

    sidebar row setup

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,3 +1,10 @@
+import { ExpandMoreOutlined } from "@material-ui/icons";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import ChatIcon from "@material-ui/icons/Chat";
+import PeopleIcon from "@material-ui/icons/People";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
 import React from "react";
 import "./Sidebar.css";
 import SidebarRow from "./SidebarRow";
@@ -5,7 +12,20 @@ import SidebarRow from "./SidebarRow";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <SidebarRow title="Pages" />
+      <SidebarRow
+        src="https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"
+        title="Paul Okello"
+      />
+      <SidebarRow
+        Icon={LocalHospitalIcon}
+        title="COVID-19 Information Center"
+      />
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messanger" />
+      <SidebarRow Icon={StorefrontIcon} title="MarketPlace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+      <SidebarRow Icon={ExpandMoreOutlined} title="Marketplace" />
     </div>
   );
 }
