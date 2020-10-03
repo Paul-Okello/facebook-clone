91cbd4f723759a0d07123597f320ddb8a51c6c84
Paul Okello <paulokello273@gmail.com>
2020-10-03 15:07:12 +0300

    Message sender component initial setup

--- src/Feed.js
+++ src/Feed.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Feed.css";
+import MessageSender from "./MessageSender";
 import StoryReal from "./StoryReal";
 
 function Feed() {
   return (
     <div className="feed">
       <StoryReal />
-      {/* <MessageSender /> */}
+      <MessageSender />
     </div>
   );
 }
