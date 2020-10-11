908d98fa506d398255fd13c78edc4909b598cfda
Paul Okello <paulokello273@gmail.com>
2020-10-11 10:23:05 +0300

    debugging the database

--- src/Feed.js
+++ src/Feed.js
@@ -6,10 +6,10 @@ import Post from "./Post";
 import StoryReal from "./StoryReal";
 
 function Feed() {
-  const [post, setPost] = useState();
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
-      setPost(
+      setPosts(
         snapshot.docs.map((doc) => ({
           id: doc.id,
           data: doc.data(),
@@ -21,20 +21,16 @@ function Feed() {
     <div className="feed">
       <StoryReal />
       <MessageSender />
-      <Post
-        profilePic="https://i.pinimg.com/564x/69/99/e3/6999e3e371c74481bc4d47252ce77ef0.jpg"
-        message="Wow this works"
-        timestamp="This is a timestamp"
-        username="Rihanna Pale"
-        image="https://c4.wallpaperflare.com/wallpaper/81/477/97/rihana-singer-wallpaper-preview.jpg"
-      />
-      <Post
-        profilePic="https://images.hivisasa.com/1200/KCjH0Et5AXOctopizzo.jpg"
-        message="Wow this works"
-        timestamp="This is a timestamp"
-        username="Namba Nane"
-        image=""
-      />
+      {posts.map((post) => (
+        <Post
+          key={post.data.id}
+          profilePic={post.data.profilePic}
+          message={post.data.message}
+          timestamp={post.data.timestamp}
+          username={post.data.username}
+          image={post.data.image}
+        />
+      ))}
     </div>
   );
 }
