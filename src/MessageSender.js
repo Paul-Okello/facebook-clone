f334d93bf3737dc5c01634179b18e3385af4d97b
Paul Okello <paulokello273@gmail.com>
2020-10-11 10:31:44 +0300

    pushing data to firestore

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -5,6 +5,8 @@ import VideocamIcon from "@material-ui/icons/Videocam";
 import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import { useStateValue } from "./StateProvider";
+import db from "./firebase";
+import firebase from "firebase";
 
 function MessageSender() {
   const [{ user }] = useStateValue();
@@ -13,6 +15,13 @@ function MessageSender() {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Some clever db stuff
+    db.collection("posts").add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      profilePic: user.photoURL,
+      username: user.displayName,
+      image: imageUrl,
+    });
     setInput("");
     setImageUrl("");
   };
